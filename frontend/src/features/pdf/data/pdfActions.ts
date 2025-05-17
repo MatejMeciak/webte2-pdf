516de0cf26a75c9c77e9b51232e122458c1d9a53
Simon Canecky <simoncanecky1@gmail.com>
2025-05-17 12:56:53 +0200

    fix

--- frontend/src/features/pdf/data/pdfActions.ts
+++ frontend/src/features/pdf/data/pdfActions.ts
@@ -4,52 +4,52 @@ import type { PdfAction } from "../types/pdf";
 export const pdfActions: PdfAction[] = [
   {
     titleKey: "merge",
-    path: "/pdf/merge",
+    path: "merge",
     icon: Combine,
   },
   {
     titleKey: "split",
-    path: "/pdf/split",
+    path: "split",
     icon: Split,
   },
   {
     titleKey: "removePage",
-    path: "/pdf/remove-page",
+    path: "remove-page",
     icon: FileX,
   },
   {
     titleKey: "extractPages",
-    path: "/pdf/extract-pages",
+    path: "extract-pages",
     icon: FileDigit,
   },
   {
     titleKey: "reorderPages",
-    path: "/pdf/reorder-pages",
+    path: "reorder-pages",
     icon: FileCog,
   },
   {
     titleKey: "addPassword",
-    path: "/pdf/add-password",
+    path: "add-password",
     icon: Lock,
   },
   {
     titleKey: "removePassword",
-    path: "/pdf/remove-password",
+    path: "remove-password",
     icon: Unlock,
   },
   {
     titleKey: "toImages", // zmenené z "pdfToImages" na "toImages"
-    path: "/pdf/to-images",
+    path: "to-images",
     icon: Image,
   },
   {
     titleKey: "addWatermark",
-    path: "/pdf/add-watermark",
+    path: "add-watermark",
     icon: Stamp,
   },
   {
     titleKey: "rotatePages",
-    path: "/pdf/rotate-pages",
+    path: "rotate-pages",
     icon: RotateCw,
   },
 ];
